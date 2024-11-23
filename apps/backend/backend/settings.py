78ec9ba10240c6f084f131cf9414d54e210d1373
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-11-23 12:11:40 -0300

    Add custom middleware for request and response logging

--- apps/backend/backend/settings.py
+++ apps/backend/backend/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 ] + REST + APPS
 
 MIDDLEWARE = [
+    'app.middlewares.CustomMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
